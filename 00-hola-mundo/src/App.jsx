56963160b526455a382ff6872d7d9d563621226c
Ubaldin Gomez <ubaldin_gomez@claro.net.do>
2023-11-11 00:34:21 -0400

    Fin Clase 1

--- 00-hola-mundo/src/App.jsx
+++ 00-hola-mundo/src/App.jsx
@@ -1,11 +1,64 @@
 import './App.css'
 import { TwitterFollowCard } from './TwitterFollowCard'
+
+const users = [
+    {
+        id: 1,
+        userName: 'midudev',
+        name: 'Miguel Angel Duran',
+        isFollowing: false
+    },
+    {
+        id: 2,
+        userName: 'umgomez',
+        name: 'Ubaldin Gomez',
+        isFollowing: true
+    },
+    {
+        id: 3,
+        userName: 'Google',
+        name: 'Google',
+        isFollowing: false
+    },
+    {
+        id: 4,
+        userName: 'NetflixLAT',
+        name: 'Netflix Latinoamerica',
+        isFollowing: true
+    },
+]
+
 export function App() {
-    const format = (userName) => `@${userName}`
     return (
         <section className='App'>
-            <TwitterFollowCard formatUsername={format} userName='umgomez' name='Ubaldin Gomez' isFollowing />
-            <TwitterFollowCard formatUsername={format} userName='mariadsd' name='Maria Gonzalez' isFollowing />
+            {
+                users.map(({ id, userName, name, isFollowing }) => (
+                    <TwitterFollowCard 
+                        key={id}
+                        userName={userName}
+                        initialIsFollowing={isFollowing}>
+                        {name}
+                    </TwitterFollowCard>
+                ))
+            }
         </section>
     )
 }
+
+/*
+    <TwitterFollowCard initialIsFollowing={true} formatUsername={format} userName='umgomez'>
+        Ubaldin Gomez
+    </TwitterFollowCard>
+
+    <TwitterFollowCard initialIsFollowing={false} formatUsername={format} userName='Google'>
+        Google
+    </TwitterFollowCard>
+
+    <TwitterFollowCard initialIsFollowing formatUsername={format} userName='NetflixLAT'>
+        Netflix Latinoamerica
+    </TwitterFollowCard>
+
+    <TwitterFollowCard initialIsFollowing={false} formatUsername={format} userName='midudev'>
+        Miguel Angel Duran
+    </TwitterFollowCard>
+*/
--- 00-hola-mundo/src/TwitterFollowCard.jsx
+++ 00-hola-mundo/src/TwitterFollowCard.jsx
@@ -1,17 +1,29 @@
-export function TwitterFollowCard({ formatUsername, userName, name, isFollowing }) {
+import { useState } from 'react'
+
+export function TwitterFollowCard({ userName, children, initialIsFollowing }) {
+    const [isFollowing, setIsFollowing] = useState(initialIsFollowing)
+
+    const textFollow = isFollowing ? 'Siguiendo' : 'Seguir'
+    const buttonClassName = isFollowing ? 'tw-followCard-button is-following' : 'tw-followCard-button'
+
+    const handleClick = () => {
+        setIsFollowing(!isFollowing)
+    }
 
     return (
         <article className='tw-followCard'>
             <header className='tw-followCard-header'>
-                <img className='tw-followCard-avatar' alt="avatar" src="/vite.svg" />
+                <img className='tw-followCard-avatar' alt="avatar" src={`https://unavatar.io/${userName}`} />
                 <div className='tw-followCard-info'>
-                    <strong>{name}</strong>
-                    <span className='tw-followCard-infoUserName'>{formatUsername(userName)}</span>
+                    <strong>{children}</strong>
+                    <span className='tw-followCard-infoUserName'>@{userName}</span>
                 </div>
             </header>
+            {/* Esta es la forma de hacer comentarios en React */}
             <aside>
-                <button className='tw-followCard-button'>
-                    Seguir
+                <button className={buttonClassName} onClick={handleClick}>
+                    <span className='tw-followCard-text'>{textFollow}</span>
+                    <span className='tw-followCard-stopFollow'>Dejar de Seguir</span>
                 </button>
             </aside>
         </article>
