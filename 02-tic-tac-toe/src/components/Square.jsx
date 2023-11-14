435b9a2f9cfb0c871b53fcac4e6cd6422ad3f67e
Ubaldin Gomez <ubaldin_gomez@claro.net.do>
2023-11-14 16:34:56 -0400

    Clase 14nov2023

---
+++ 02-tic-tac-toe/src/components/Square.jsx
@@ -0,0 +1,14 @@
+//Componente
+export const Square = ({ children, isSelected, index, updateBoard }) => {
+    const className = `square ${isSelected ? 'is-selected' : ''}`
+
+    const handleClick = () => {
+        updateBoard(index)
+    }
+
+    return (
+        <div onClick={handleClick} className={className}>
+            {children}
+        </div>
+    )
+}
