5f9e423395dea24946360e6ba8f42edb34c067aa
Ubaldin Gomez <ubaldin_gomez@claro.net.do>
2023-11-15 10:29:21 -0400

    Guardar en localstorage

--- 02-tic-tac-toe/src/App.jsx
+++ 02-tic-tac-toe/src/App.jsx
@@ -5,10 +5,18 @@ import { Square } from './components/Square.jsx'
 import { TURNS } from "./constants.js"
 import { checkWinnerFrom, checkEndGame } from './logic/board.js'
 import { WinnerModal } from "./components/WinnerModal.jsx"
+import { saveGameToStorage, resetGameStorage } from './logic/storage/index.js'
 
 function App() {
-  const [board, setBoard] = useState(Array(9).fill(null))
-  const [turn, setTurn] = useState(TURNS.X)
+  const [board, setBoard] = useState(() => {
+    const boardFromStorage = window.localStorage.getItem('board')
+    if (boardFromStorage) return JSON.parse(boardFromStorage)
+    return Array(9).fill(null)
+  })
+  const [turn, setTurn] = useState(() => {
+    const turnFromStorage = window.localStorage.getItem('turn')
+    return turnFromStorage ?? TURNS.X
+  })
   // null es que no hay ganador, false es que hay un empate
   const [winner, setWinner] = useState(null)
 
@@ -24,6 +32,9 @@ function App() {
     // cambiar el turno
     const newTurn = turn == TURNS.X ? TURNS.O : TURNS.X
     setTurn(newTurn)
+    // guardar la partida
+    saveGameToStorage({ board: newBoard, turn: newTurn })
+
     // revisar si hay un ganador
     const newWinner = checkWinnerFrom(newBoard)
     if (newWinner) {
@@ -38,6 +49,8 @@ function App() {
     setBoard(Array(9).fill(null))
     setTurn(TURNS.X)
     setWinner(null)
+
+    resetGameStorage()
   }
 
   return (
@@ -48,11 +61,7 @@ function App() {
         {
           board.map((square, index) => {
             return (
-              <Square
-                key={index}
-                index={index}
-                updateBoard={updateBoard}
-              >
+              <Square key={index} index={index} updateBoard={updateBoard}>
                 {square}
               </Square>
             )
